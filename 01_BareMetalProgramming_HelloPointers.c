01513ae6affba990cfd5082025f891b3f3470003
ChrisGoku <chrisgrillo614@gmail.com>
2025-01-25 16:42:56 -0500

    added space

--- 01_BareMetalProgramming_HelloPointers.c
+++ 01_BareMetalProgramming_HelloPointers.c
@@ -4,7 +4,7 @@ int main() {
     int x = 42; 
     int y = 50;
     int *p = &x;  
-    int *b = &y;
+    int *b = &y; 
 
     printf("x: %d\n", x);      // Value of x
     printf("*p: %d\n", *p);      
