5a9694414d9a93be590f2579e28582c7a5244b6d
ChrisGoku <chrisgrillo614@gmail.com>
2025-01-25 16:19:03 -0500

    First Commit

---
+++ 01_BareMetalProgramming_HelloPointers.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main() { 
+    int x = 42; 
+    int y = 50;
+    int *p = &x;  
+    int *b = &y;
+
+    printf("x: %d\n", x);      // Value of x
+    printf("*p: %d\n", *p);      
+    printf("*b: %d\n", *b);      
+
+
+
+    return 0; 
+} 
